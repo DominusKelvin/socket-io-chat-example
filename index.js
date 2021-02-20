d6ddd6e13a66b87299544efa477d600001fa20b2
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-20 11:45:03 +0100

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connect', (socket) => {
+  console.log('realtime user connected')
+ socket.on('chat message', (message) => {
+	 io.emit('chat message', message)
+ })
+
+socket.on('disconnect', () => {
+	  console.log('realtime user disconnected')
+  })
+
+})
+
+
+
+http.listen(3000, () => {
+  console.log("listening on *:3000")
+})
